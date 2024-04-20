45281e271db4f54c404a94c83c3aa7e8ca05529c
David.Yucra-Mamani <103071814+yucradavid@users.noreply.github.com>
2024-04-19 21:21:16 -0500

    Ingresando el ms-matriculas rosaura
    Ingresando el ms-matriculas rosaura

---
+++ ms-matriculas/src/test/java/com/example/msmatriculas/MsMatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msmatriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
