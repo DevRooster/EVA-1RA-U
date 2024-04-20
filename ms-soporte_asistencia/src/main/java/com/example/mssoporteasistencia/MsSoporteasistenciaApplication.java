05f21cac3d66e18418d3e38bc4bf76219ff87fec
david yucra mamani <davidrobertyucramamani@gmail.com>
2024-04-20 17:23:00 -0500

    completo y retoques

--- ms-soporte_asistencia/src/main/java/com/example/mssoporteasistencia/MsSoporteasistenciaApplication.java
+++ ms-soporte_asistencia/src/main/java/com/example/mssoporteasistencia/MsSoporteasistenciaApplication.java
@@ -1,7 +1,11 @@
 package com.example.mssoporteasistencia;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsSoporteasistenciaApplication {
@@ -9,5 +13,14 @@ public class MsSoporteasistenciaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsSoporteasistenciaApplication.class, args);
 	}
-
+	@Bean
+	public OpenAPI custumOpenAPI(){
+		return new OpenAPI().info (new Info()
+				.title("OPEN API MICROSERVICIO SOPORTE Y ASISTENCIA TECNICA")
+				.version("0.0.1")
+				.description("servicio web soporte_asistencia")
+				.termsOfService("http://swagger.io/terms")
+				.license (new License().name("Apache 2.0").url("http://springdoc.org"))
+		);
+	}
 }
