a8901c04370321516c043a6759b6ff5db9d1d975
david yucra mamani <davidrobertyucramamani@gmail.com>
2024-04-20 15:12:26 -0500

    ms-integracion_instituciones, completo
    Completo

--- ms-integracion_instituciones/src/main/java/com/example/msinstitucion_externa/controller/DatosCompartidosController.java
+++ ms-integracion_instituciones/src/main/java/com/example/msinstitucion_externa/controller/DatosCompartidosController.java
@@ -1,4 +1,42 @@
 package com.example.msinstitucion_externa.controller;
 
+import com.example.msinstitucion_externa.entity.DatosCompartidos;
+import com.example.msinstitucion_externa.service.DatosCompartidosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/DatosCompartidos")
 public class DatosCompartidosController {
+    @Autowired
+    private DatosCompartidosService datosCompartidosService;
+
+    @GetMapping
+    ResponseEntity<List<DatosCompartidos>> lista(){
+        return ResponseEntity.ok(datosCompartidosService.lista());
+    }
+    @PostMapping
+    ResponseEntity<DatosCompartidos> guardar(@RequestBody DatosCompartidos DatosCompartidos){
+        return ResponseEntity.ok(datosCompartidosService.guardar((DatosCompartidos)));
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<DatosCompartidos> buscarPorId(@PathVariable(required = true) Integer id){
+        return ResponseEntity.ok(datosCompartidosService.buscarPorId(id).get());
+
+    }
+
+    @PutMapping
+    ResponseEntity<DatosCompartidos> actualizar(@RequestBody DatosCompartidos DatosCompartidos){
+        return ResponseEntity.ok(datosCompartidosService.actualizar((DatosCompartidos)));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<DatosCompartidos>> eleminar(@PathVariable(required = true) Integer id){
+        datosCompartidosService.eleminar(id);
+        return ResponseEntity.ok(datosCompartidosService.lista());
+
+    }
 }
--- ms-integracion_instituciones/src/main/java/com/example/msinstitucion_externa/controller/InstitucionExternaController.java
+++ ms-integracion_instituciones/src/main/java/com/example/msinstitucion_externa/controller/InstitucionExternaController.java
@@ -1,4 +1,42 @@
 package com.example.msinstitucion_externa.controller;
 
+import com.example.msinstitucion_externa.entity.InstitucionExterna;
+import com.example.msinstitucion_externa.service.InstitucionExternaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/InstitucionExterna")
 public class InstitucionExternaController {
+    @Autowired
+    private InstitucionExternaService institucionExternaService;
+
+    @GetMapping
+    ResponseEntity<List<InstitucionExterna>> lista(){
+        return ResponseEntity.ok(institucionExternaService.lista());
+    }
+    @PostMapping
+    ResponseEntity<InstitucionExterna> guardar(@RequestBody InstitucionExterna institucionExterna){
+        return ResponseEntity.ok(institucionExternaService.guardar((institucionExterna)));
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<InstitucionExterna> buscarPorId(@PathVariable(required = true) Integer id){
+        return ResponseEntity.ok(institucionExternaService.buscarPorId(id).get());
+
+    }
+
+    @PutMapping
+    ResponseEntity<InstitucionExterna> actualizar(@RequestBody InstitucionExterna institucionExterna){
+        return ResponseEntity.ok(institucionExternaService.actualizar((institucionExterna)));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<InstitucionExterna>> eleminar(@PathVariable(required = true) Integer id){
+        institucionExternaService.eleminar(id);
+        return ResponseEntity.ok(institucionExternaService.lista());
+
+    }
 }
