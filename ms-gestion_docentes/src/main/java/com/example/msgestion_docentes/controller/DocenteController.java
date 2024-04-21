1dd03e572934059f4c965179298214ae974a1f0c
rosaura <rosaurayanapari123@gmail.com>
2024-04-21 01:09:06 -0500

    completando

--- ms-gestion_docentes/src/main/java/com/example/msgestion_docentes/controller/DocenteController.java
+++ ms-gestion_docentes/src/main/java/com/example/msgestion_docentes/controller/DocenteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/solicitudSoporte")
+@RequestMapping("/Docente")
 public class DocenteController {
     @Autowired
     private DocenteService docenteService;
--- ms-gestion_eventos_actividades/src/main/java/com/example/msgestion_eventos_actividades/controller/RecursoNecesarioController.java
+++ ms-gestion_eventos_actividades/src/main/java/com/example/msgestion_eventos_actividades/controller/RecursoNecesarioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/OtraUgel")
+@RequestMapping("/RecursoNecesarios")
 public class RecursoNecesarioController {
     @Autowired
     private RecursoNecesarioService recursoNecesarioService;
