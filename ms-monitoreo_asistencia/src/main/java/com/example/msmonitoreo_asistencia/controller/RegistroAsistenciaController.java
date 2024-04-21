da00c0f5a5e6155651f431ec4ec632d9f1f11850
David.Yucra-Mamani <davidrobertyucramamani@gmail.com>
2024-04-21 14:20:34 -0500

    se areglo los inperfectos para el postman

--- ms-monitoreo_asistencia/src/main/java/com/example/msmonitoreo_asistencia/controller/RegistroAsistenciaController.java
+++ ms-monitoreo_asistencia/src/main/java/com/example/msmonitoreo_asistencia/controller/RegistroAsistenciaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/RegistroAsistencia")
+@RequestMapping("/registro_asistencia")
 public class RegistroAsistenciaController {
     @Autowired
     private RegistroAsistenciaService registroAsistenciaService;
