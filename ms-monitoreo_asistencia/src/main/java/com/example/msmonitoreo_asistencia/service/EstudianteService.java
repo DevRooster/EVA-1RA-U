a15e936ecc8d08ddfec8b6ba3e548a80a41728cc
David.Yucra-Mamani <davidrobertyucramamani@gmail.com>
2024-04-21 01:26:50 -0500

    ms config

--- ms-monitoreo_asistencia/src/main/java/com/example/msmonitoreo_asistencia/service/EstudianteService.java
+++ ms-monitoreo_asistencia/src/main/java/com/example/msmonitoreo_asistencia/service/EstudianteService.java
@@ -1,10 +1,11 @@
 package com.example.msmonitoreo_asistencia.service;
 
 import com.example.msmonitoreo_asistencia.entity.Estudiante;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public interface EstudianteService {
     List<Estudiante> lista();
     Estudiante guardar(Estudiante estudiante);
