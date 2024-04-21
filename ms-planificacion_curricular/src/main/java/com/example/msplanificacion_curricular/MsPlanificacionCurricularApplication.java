c4b2fc4cd604b9b129d991b796621d51bbd64636
Gladys Rosaura Yana Pari <rosaurayanapari123@gmail.com>
2024-04-20 21:10:54 -0500

    ms-planificasion casi compkleto

--- ms-planificacion_curricular/src/main/java/com/example/msplanificacion_curricular/MsPlanificacionCurricularApplication.java
+++ ms-planificacion_curricular/src/main/java/com/example/msplanificacion_curricular/MsPlanificacionCurricularApplication.java
@@ -1,7 +1,11 @@
 package com.example.msplanificacion_curricular;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsPlanificacionCurricularApplication {
@@ -9,5 +13,14 @@ public class MsPlanificacionCurricularApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsPlanificacionCurricularApplication.class, args);
 	}
-
+	@Bean
+	public OpenAPI custumOpenAPI(){
+		return new OpenAPI().info (new Info()
+				.title("OPEN API MICROSERVICIO PLANIFICACION CURRICULAR")
+				.version("0.0.1")
+				.description("servicio web planificacion_curricular")
+				.termsOfService("http://swagger.io/terms")
+				.license (new License().name("Apache 2.0").url("http://springdoc.org"))
+		);
+	}
 }
