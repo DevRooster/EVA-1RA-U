0dc571444846e543c01907355f343d40e36412f5
Gladys Rosaura Yana Pari <rosaurayanapari123@gmail.com>
2024-04-20 19:05:15 -0500

    creando microservicio ms-planificacion_curricular,
    solo es la creacion de microservicio esta vacio

---
+++ ms-planificacion_curricular/src/main/java/com/example/msplanificacion_curricular/MsPlanificacionCurricularApplication.java
@@ -0,0 +1,13 @@
+package com.example.msplanificacion_curricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPlanificacionCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPlanificacionCurricularApplication.class, args);
+	}
+
+}
---
+++ ms-planificacion_curricular/src/test/java/com/example/msplanificacion_curricular/MsPlanificacionCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msplanificacion_curricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPlanificacionCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
